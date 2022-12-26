7e6dfb31ac37849e918eeb69347b853962170512
Pankaj Parkar <parkarpankaj3@gmail.com>
2022-12-27 01:38:07 +0800

    Footer added color

--- src/app/common/footer/footer.component.ts
+++ src/app/common/footer/footer.component.ts
@@ -6,11 +6,20 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   template: `
-    <p>
-      footer works!
-    </p>
+    <footer>
+      Test
+    </footer>
   `,
   styles: [
+    `
+    @use '@angular/material' as mat;
+    $deep-purple-palette: mat.$deep-purple-palette;
+
+    footer {
+      color: red;
+      background: mat.get-color-from-palette($deep-purple-palette, 500);
+    }
+    `
   ]
 })
 export class FooterComponent {
