c78d0914027f3fe31c8bca694378ddc25428ccfe
Pankaj Parkar <parkarpankaj3@gmail.com>
2022-12-27 00:30:42 +0800

    Added navbar toolbar

--- src/app/common/navbar/navbar.component.ts
+++ src/app/common/navbar/navbar.component.ts
@@ -1,16 +1,37 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'cf-navbar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
   template: `
-    <p>
-      navbar works!
-    </p>
+    <mat-toolbar color="primary">
+      <!-- <button mat-icon-button class="example-icon" aria-label="Example icon-button with menu icon">
+        <mat-icon>menu</mat-icon>
+      </button> -->
+      <span>NgIndia Feedback</span>
+      <span class="example-spacer"></span>
+      <button mat-icon-button class="example-icon favorite-icon" aria-label="Example icon-button with heart icon">
+        <mat-icon>favorite</mat-icon>
+      </button>
+      <button mat-icon-button class="example-icon" aria-label="Example icon-button with share icon">
+        <mat-icon>share</mat-icon>
+      </button>
+    </mat-toolbar>
   `,
   styles: [
+    `
+    .example-spacer {
+      flex: 1 1 auto;
+    }
+    `
   ]
 })
 export class NavbarComponent {
