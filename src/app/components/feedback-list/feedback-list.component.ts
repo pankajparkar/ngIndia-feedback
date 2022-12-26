98ec9d705b4b957e8dc24abb26c94d8a4973dcb5
Pankaj Parkar <parkarpankaj3@gmail.com>
2022-12-27 00:34:18 +0800

    Added feedback list component.

---
+++ src/app/components/feedback-list/feedback-list.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'cf-feedback-list',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <p>
+      feedback-list works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class FeedbackListComponent {
+
+}
