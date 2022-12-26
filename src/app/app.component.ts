e4899951811b21369439e0b376c194dc7ec9511f
Pankaj Parkar <parkarpankaj3@gmail.com>
2022-12-26 22:03:22 +0800

    Added basic layout.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './common/footer/footer.component';
+import { NavbarComponent } from './common/navbar/navbar.component';
 
 @Component({
   selector: 'cf-root',
-  template: `
-    Test
-  `,
   standalone: true,
+  template: `
+    <cf-navbar></cf-navbar>
+    <router-outlet></router-outlet>
+    <cf-footer></cf-footer>
+  `,
+  imports: [
+    NavbarComponent,
+    RouterOutlet,
+    FooterComponent
+  ],
 })
 export class AppComponent {
 }
