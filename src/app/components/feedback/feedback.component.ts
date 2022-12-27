782807d2467d59e2cb5378103bb3d94a6d26a6b7
Pankaj Parkar <parkarpankaj3@gmail.com>
2022-12-27 23:23:38 +0800

    Added switchcase for feedback.

--- src/app/components/feedback/feedback.component.ts
+++ src/app/components/feedback/feedback.component.ts
@@ -1,7 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { QuestionComponent } from '../question/question.component';
+import { NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
+import { WelcomeComponent } from '../welcome/welcome.component';
+import { ThanksComponent } from '../thanks/thanks.component';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'cf-feedback',
@@ -10,6 +14,11 @@ import { QuestionComponent } from '../question/question.component';
     MatCardModule,
     MatButtonModule,
     QuestionComponent,
+    NgSwitchCase,
+    NgSwitchDefault,
+    NgSwitch,
+    WelcomeComponent,
+    ThanksComponent,
   ],
   template: `
     <div class="step-container">
@@ -19,6 +28,17 @@ import { QuestionComponent } from '../question/question.component';
       <div class="step-subtitle">
         {{ step.stepSubtitle }}
       </div>
+      <ng-container *ngSwitch="step.type">
+        <ng-container *ngSwitchCase="'welcome'">
+          <cf-welcome></cf-welcome>
+        </ng-container>
+        <ng-container *ngSwitchCase="'welcome'">
+          <cf-thanks></cf-thanks>
+        </ng-container>
+        <ng-container *ngSwitchDefault>
+          <cf-question></cf-question>
+        </ng-container>
+      </ng-container>
       <cf-question></cf-question>
     </div>
 
@@ -41,12 +61,32 @@ import { QuestionComponent } from '../question/question.component';
   ]
 })
 export class FeedbackComponent {
-  step = {
+  steps = [{
     stepTitle: 'Step Title',
     stepSubtitle: 'Step Subtitle',
     cardTitle: 'Card Title',
     cardSubtitle: 'Card Subtitle',
     type: 'start',
     questionId: 1,
-  };
+  }, {
+    stepTitle: 'Step Title',
+    stepSubtitle: 'Step Subtitle',
+    cardTitle: 'Card Title',
+    cardSubtitle: 'Card Subtitle',
+    type: 'start',
+    questionId: 1,
+  }, {
+    stepTitle: 'Step Title',
+    stepSubtitle: 'Step Subtitle',
+    cardTitle: 'Card Title',
+    cardSubtitle: 'Card Subtitle',
+    type: 'start',
+    questionId: 1,
+  }];
+  route = inject(ActivatedRoute);
+  id = +(this.route.snapshot.params['id'] ?? 1);
+  isFirstStep = this.id === 1;
+  isLastStep = this.id === this.steps.length;
+  isQuestion = !this.isFirstStep && !this.isLastStep;
+  step = this.steps[this.id];
 }
