5f1627e2634c0377e8a1ab98c95198e584e0451c
Pankaj Parkar <parkarpankaj3@gmail.com>
2022-12-27 21:52:02 +0800

    Added cards on dashbaord.

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,18 +1,42 @@
+import { NgForOf } from '@angular/common';
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'cf-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    MatCardModule,
+    NgForOf,
+  ],
   template: `
-    <p>
-      dashboard works!
-    </p>
+    <mat-card *ngFor="let stat of stats">
+      <mat-card-title>
+        {{ stat.title }}
+      </mat-card-title>
+    </mat-card>
   `,
   styles: [
+    `
+    :host {
+      display: flex;
+      flex-flow: row wrap;
+      mat-card {
+        margin: 16px;
+        width: 200px;
+      }
+    }
+    `
   ]
 })
 export class DashboardComponent {
-
+  stats: any[] = [
+    { title: 'Overall' },
+    { title: 'Talk 1' },
+    { title: 'Talk 2' },
+    { title: 'Talk 3' },
+    { title: 'Talk 4' },
+    { title: 'Talk 5' },
+    { title: 'Talk 6' },
+  ];
 }
