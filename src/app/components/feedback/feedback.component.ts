1ad4f6dedf0dd4d81bb9dd0bd785476784a818db
Pankaj Parkar <parkarpankaj3@gmail.com>
2022-12-27 22:16:09 +0800

    Mat card with wizard.

--- src/app/components/feedback/feedback.component.ts
+++ src/app/components/feedback/feedback.component.ts
@@ -1,18 +1,51 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'cf-feedback',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+  ],
   template: `
-    <p>
-      feedback works!
-    </p>
+    <mat-card>
+      <mat-card-title>
+        {{ step.title }}
+      </mat-card-title>
+      <mat-card-content>
+        My Content
+      </mat-card-content>
+      <mat-card-footer>
+        <button mat-raised-button type="button" color="primary">
+          Start
+        </button>
+        <button mat-raised-button type="button" color="primary">
+          Prev
+        </button>
+        <button mat-raised-button type="button" color="primary">
+          Next
+        </button>
+        <button mat-raised-button type="button" color="primary">
+          End
+        </button>
+      </mat-card-footer>
+    </mat-card>
   `,
   styles: [
+    `
+    button {
+      margin: 0 5px;
+    }
+    `
   ]
 })
 export class FeedbackComponent {
-
+  step = {
+    title: 'Step Title',
+    type: 'start',
+    questionId: 1,
+  }
 }
